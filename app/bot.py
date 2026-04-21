cdbc3622d337215f0d1c122ec2701e1bb5eca269
17coinmail-afk <17coinmail@gmail.com>
2026-04-21 13:03:54 +0800

    Fix aiogram 3.7+ Bot initializer

--- app/bot.py
+++ app/bot.py
@@ -1,6 +1,10 @@
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from app.config import config
 
-bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(
+    token=config.BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+)
 dp = Dispatcher()
